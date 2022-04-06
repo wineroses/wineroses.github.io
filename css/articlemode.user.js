83206dcb416006c483644d4961f5225e0d4e5ab2
wineroses <wineroses@users.noreply.github.com>
2022-04-06 20:39:52 +0900

    Update articlemode.user.js

--- css/articlemode.user.js
+++ css/articlemode.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name        Articlemode for Dynalist
-// @namespace   wineroses
-// @description article-mode
-// @include     https://dynalist.io/*
-// @version     1
-// @grant       none
+// @name         Articlmode for Dynalist
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Dynalistのアーティクルビューの行間を狭める
+// @author       wineroses
+// @match        https://dynalist.io/d/*
+// @grant        none
 // ==/UserScript==
 
-  Css=document.createElement('style');
-  Css.innerText=".is-compactDensity.is-article-view .Node-self, .is-cozyDensity.is-article-view .Node-self,
+Css = document.createElement('style');
+Css.innerText = ".is-compactDensity.is-article-view .Node-self, .is-cozyDensity.is-article-view .Node-self,
  .is-comfortableDensity.is-article-view .Node-self{padding-top:0px;padding-bottom:3px;}";
-  document.head.appendChild(Css);
+document.head.appendChild(Css);
