81f48aa2212c0ec915183ddfcac4810a97bc74a0
wineroses <wineroses@users.noreply.github.com>
2022-04-06 19:56:27 +0900

    Update articlemode.js

--- css/articlemode.js
+++ css/articlemode.js
@@ -1,6 +1,13 @@
-if(/https:\/\/dynalist\/.io/.test(location.href)){
+// ==UserScript==
+// @name        Articlemode for Dynalist
+// @namespace   wineroses
+// @description article-mode
+// @include     https://dynalist.io/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
   Css=document.createElement('style');
   Css.innerText=".is-compactDensity.is-article-view .Node-self, .is-cozyDensity.is-article-view .Node-self,
  .is-comfortableDensity.is-article-view .Node-self{padding-top:0px;padding-bottom:3px;}";
   document.head.appendChild(Css);
-}
