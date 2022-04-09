59364ee0bb11efc86277550661e4f3c752ef9e1d
wineroses <wineroses@users.noreply.github.com>
2022-04-09 17:32:16 +0900

    Update Dynalist-to-Textwell.user.js

--- css/Dynalist-to-Textwell.user.js
+++ css/Dynalist-to-Textwell.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dynalist to Textwell
 // @version      1
-// @description  DynalistをTextwellに出力
+// @description  DynalistをTextwell に出力
 // @author       wineroses
 // @namespace    https://wineroses.hatenablog.com/
 // @match        https://dynalist.io/d/*
@@ -9,18 +9,19 @@
 
 e = document.createElement("div");
 e.id = "TextOut";
-e.innerHTML = "■";
-e.style.cssText = "position:fixed;top:68px;right:20px;color: darkred; ";
-document.body.appendChild(e);
+e.innerHTML = " [T] ";
+e.style.cssText = "display:block; padding:8px; color:whitesmoke;";
+p = document.querySelector(".node-toolbar-options-container");
+p.appendChild(e);
 
 TextOut.onclick = function(){
-  p = document.querySelectorAll(".Node:not([style*='display:none;']) .is-parent,.is-currentRoot .Node-content");
+  p = document.querySelectorAll(".Node .is-parent,.is-currentRoot .Node-content");
   s = "";
   for(i=0; i<p.length; i++){
     if(p[i].classList.contains("is-parent")){
       s+= "\n";
     }else{
-      if(p[i].style.display!="invisible") s+= p[i].innerText;
+      if(p[i].style.display!="none") s+= p[i].innerText;
     }
   }
   s = s.replace(/^\n+/, "");
