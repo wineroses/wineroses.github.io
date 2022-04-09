0f2d9d37b23b69a8df81c36bbcb1c44ffbd64490
wineroses <wineroses@users.noreply.github.com>
2022-04-09 15:16:33 +0900

    Create Dynalist-to-Obsidian.user.js

---
+++ css/Dynalist-to-Obsidian.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Dynalist to Obsidian
+// @version      1
+// @description  DynalistをObsidian に出力
+// @author       wineroses
+// @namespace    https://wineroses.hatenablog.com/
+// @match        https://dynalist.io/d/*
+// ==/UserScript==
+
+e = document.createElement("div");
+e.id = "TextOut";
+e.innerHTML = " [O] ";
+e.style.cssText = "display:block; padding:8px; color:whitesmoke;";
+p = document.querySelector(".node-toolbar-options-container");
+p.appendChild(e);
+
+TextOut.onclick = function(){
+  p = document.querySelectorAll(".Node .is-parent,.is-currentRoot .Node-content");
+  s = "";
+  for(i=0; i<p.length; i++){
+    if(p[i].classList.contains("is-parent")){
+      s+= "\n";
+    }else{
+      if(p[i].style.display!="none") s+= p[i].innerText;
+    }
+  }
+  s = s.replace(/^\n+/, "");
+  s = encodeURIComponent(s);
+  url = "obsidian://new?content=" + s + "&file=" + encodeURIComponent(document.title);
+  window.open(url);
+}
